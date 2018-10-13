278d1373d95a7ab847b19d3c63a99f2e0d2bd5b6
Jake Zaia <jlz2369@g.rit.edu>
2018-10-12 20:34:24 -0400

    Add aliases

---
+++ scripts/gitAliases.sh
@@ -0,0 +1,9 @@
+#Created by Jake Zaia
+
+#The contents of this file should be copied to ~/.bash_aliases
+  #Do NOT run this file as a script, it will do nothing.
+
+alias aa='git add .'
+alias pull='git pull && ls'
+alias push='git push'
+alias commit='git commit -am '
